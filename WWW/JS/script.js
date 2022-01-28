600ab4b47433067709e30ea19bcc284fb2a75d91
Mak5Mak5er <98599732+Mak5Mak5er@users.noreply.github.com>
2022-01-28 16:33:59 +0200

    Add files via upload

---
+++ WWW/JS/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    $(".btn").on("click", function() {
+        var text = $(".text-field")
+        var value = text.val()
+        var btnValue = $(this).val()
+
+        function add(v) {
+            text.val(value + v);
+        }
+        switch (btnValue) {
+            case "C":
+                text.val("0")
+                break;
+            case "←":
+                if (value.length > 1) {
+                    text.val(value.slice(0, -1));
+                } else text.val("0")
+                break;
+            case ".":
+                if (!value.includes(".")) add(".")
+                break;
+            case "0":
+                if (value !== '0') add("0")
+                break;
+            default:
+                if (value === "0") {
+                    text.val(btnValue)
+                } else
+                if (value.length < 25) {
+                    add(btnValue)
+                }
+
+        }
+    })
+
+});
